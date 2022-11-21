1e8190b35c453a49e3a8f67ba76efe62e48b323d
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-11-21 13:20:12 +0200

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import router from './router'
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router}/>
   </Provider>
 )
